b7ec250ceeea9d01bd99bafe4ec3628a9dc8fee9
brcevss <bricevsl@outlook.fr>
2025-01-11 18:12:58 +0100

    Initialisation du projet Spring

---
+++ src/main/java/com/brcevss/cv_generator_batch/CvGeneratorBatchApplication.java
@@ -0,0 +1,13 @@
+package com.brcevss.cv_generator_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvGeneratorBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvGeneratorBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brcevss/cv_generator_batch/CvGeneratorBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brcevss.cv_generator_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvGeneratorBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
